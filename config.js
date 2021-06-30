1f17404f8a6a42684b52a3787a0f72f10d2ccd0a
tuong15456 <86703287+tuong15456@users.noreply.github.com>
2021-06-30 11:52:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Ngọc ơiiiiiiiiii, Ngọc àaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!',
+    introTitle: 'Ngọc ơiiiiiiiiii, Ngọc àaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
