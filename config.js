02589128c3eac3637a9027d88f4b6561922d1bc7
tuong15456 <86703287+tuong15456@users.noreply.github.com>
2021-06-30 11:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ const CONFIG = {
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho anh yêu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hẹn em bé hết đợt dịch này nha, em bé của anh ơiiii, sẽ dẫn em bé đi ăn hết sài gòn luôn.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
